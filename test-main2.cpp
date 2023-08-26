895ee14b31e21a62034bf5548152d7a12b5f5e4b
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-08-26 21:39:27 +0300

    Добавьте файлы проекта.

--- test-main2.cpp
+++ test-main2.cpp
@@ -4,27 +4,43 @@
 #include "testHeader2.h"
 
 TEST_CASE("test func", "[Func]") {
-	SECTION("testing") {
-		List list;
+	List list;
+	list.Empty();
+	SECTION("testing func") 
+	{
 		CHECK(list.Empty() == 1);
 		CHECK(list.Size() == 0);
+	}
+	SECTION("Добавление 2-х элементов и проверка размера")
+	{
 		list.PushBack(10);
 		CHECK(list.Size() == 1);
 		list.PushFront(20);
 		CHECK(list.Size() == 2);
-		list.PopBack();
+	}
+	SECTION("Добавление и удаление первого и последнего элемента, проверка размера")
+	{
+		list.PushFront(1);
+		list.PushBack(2);
 		list.PopFront();
+		list.PopBack();
 		CHECK(list.Size() == 0);
+	}
+	SECTION("Добавление по одному элементу в начало и конецб, проверка размера")
+	{
 		list.PushBack(10);
 		list.PushFront(20);
 		CHECK(list.Size() == 2);
+	}
+	SECTION("Очистка через функцию, проверка размера")
+	{
 		list.Clear();
 		CHECK(list.Size() == 0);
 	}
-
-}
+	}
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	return Catch::Session().run();
 }
